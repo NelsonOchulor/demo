7084a60985e5a362e58b1f3a717c1e5bbef0b56e
Nelson Ochulor <ugonelly2002@yahoo.com>
2022-06-27 14:21:58 -0400

    Create Create3_bkup.py
    Updated Project1 file_1. Initial file deleted

---
+++ Create3_bkup.py
@@ -0,0 +1,27 @@
+import mysql.connector
+# established connection between your database
+mysqldb = mysql.connector.connect(
+    host="localhost", user="root", password="", database="project1")
+mycursor = mysqldb.cursor()  # cursor() method create a cursor object
+
+first_name = input('Enter First Name = ')
+last_name = input('Enter Last Name = ')
+birth_date = input('Enter Birth Date = ')
+address = input('Enter Address = ')
+city = input('Enter City = ')
+state = input('Enter State = ')
+post_code = str(input('Enter Post Code = '))
+contact = input('Enter Contact Number = ')
+email = input('Enter email = ')
+
+try:
+    mycursor.execute(
+        f"INSERT INTO personal_record VALUES (default, '{first_name}', '{last_name}', '{birth_date}', '{address}', '{city}', '{state}', '{post_code}', '{contact}', '{email}')")
+
+    mysqldb.commit()  # Commit is used for your changes in the database
+    print('Personal Record Data Saved Successfully')
+
+except:
+    print('Something wrong with Personal Record, please check')
+
+mysqldb.close()  # Connection Close
